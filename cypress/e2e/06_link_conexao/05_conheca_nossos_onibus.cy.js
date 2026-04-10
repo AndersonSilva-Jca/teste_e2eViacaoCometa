a60e8eed1dd00907c1ee304bebc30dce6ec3e763
Anderson Silva dos Santos <anderson.ssantos@jcatlm.com.br>
2026-04-10 20:16:23 -0300

    Update 05_conheca_nossos_onibus.cy.js

--- cypress/e2e/06_link_conexao/05_conheca_nossos_onibus.cy.js
+++ cypress/e2e/06_link_conexao/05_conheca_nossos_onibus.cy.js
@@ -1,18 +1,18 @@
-/// <reference types='cypress' />
+// /// <reference types='cypress' />
 
 
-describe('Validar link de conexão com os ônibus', () => {
-  beforeEach(() => {
-    cy.intercept({ resourceType: /xhr|fetch/ }, { log: false });
-    cy.visit('https://www.viacaocometa.com.br');
+// describe('Validar link de conexão com os ônibus', () => {
+//   beforeEach(() => {
+//     cy.intercept({ resourceType: /xhr|fetch/ }, { log: false });
+//     cy.visit('https://www.viacaocometa.com.br');
 
-  });
-  it('Deve validar o link do card de conexão com os ônibus e redirecionar para a página de contato', () => {
-    cy.get('#headingitem0').click()
-    cy.get('#headingitem1').click()
-    cy.get('#headingitem2').click()
-    cy.get('#headingitem3').click()
-    cy.get('[style="color: rgb(255,0,150);"] > .focusable > span').click({ force: true });
-    cy.url().should('include', 'fale-conosco')
-  })
-});
+//   });
+//   it('Deve validar o link do card de conexão com os ônibus e redirecionar para a página de contato', () => {
+//     cy.get('#headingitem0').click()
+//     cy.get('#headingitem1').click()
+//     cy.get('#headingitem2').click()
+//     cy.get('#headingitem3').click()
+//     cy.get('[style="color: rgb(255,0,150);"] > .focusable > span').click({ force: true });
+//     cy.url().should('include', 'fale-conosco')
+//   })
+// });
