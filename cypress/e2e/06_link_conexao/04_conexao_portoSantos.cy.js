7aa9f9da274ebebb19409d536c8f4d1403b34aeb
Anderson Silva dos Santos <anderson.ssantos@jcatlm.com.br>
2026-04-10 20:16:12 -0300

    Comment out test for Porto de Santos connection link

--- cypress/e2e/06_link_conexao/04_conexao_portoSantos.cy.js
+++ cypress/e2e/06_link_conexao/04_conexao_portoSantos.cy.js
@@ -1,14 +1,14 @@
-/// <reference types='cypress' />
+// /// <reference types='cypress' />
 
-describe('Validar link de conexão com o Porto de Santos', () => {
-  beforeEach(() => {
-    cy.intercept({ resourceType: /xhr|fetch/ }, { log: false });
-    cy.visit('https://www.viacaocometa.com.br');
-  });
-  it('Deve validar o link do card de conexão com o Porto de Santos e redirecionar para a página de conexão com o Porto de Santos', () => {
-    cy.get('[data-tab="slick_tabs-content-carousel-2"]').click({ force: true });
-    cy.get('#slick-slide90 > .carousel-cards-content-text > .aem-Grid > .button > clientlib > .about-card-button > .focusable').click({ force: true });
-    cy.url().should('include', '/destinos/porto-santos')
+// describe('Validar link de conexão com o Porto de Santos', () => {
+//   beforeEach(() => {
+//     cy.intercept({ resourceType: /xhr|fetch/ }, { log: false });
+//     cy.visit('https://www.viacaocometa.com.br');
+//   });
+//   it('Deve validar o link do card de conexão com o Porto de Santos e redirecionar para a página de conexão com o Porto de Santos', () => {
+//     cy.get('[data-tab="slick_tabs-content-carousel-2"]').click({ force: true });
+//     cy.get('#slick-slide90 > .carousel-cards-content-text > .aem-Grid > .button > clientlib > .about-card-button > .focusable').click({ force: true });
+//     cy.url().should('include', '/destinos/porto-santos')
 
-  })
-});
+//   })
+// });
