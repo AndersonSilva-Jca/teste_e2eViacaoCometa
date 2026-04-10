44a4715d7589585c69f9fb1070d83b679b9e235b
Anderson Silva dos Santos <anderson.ssantos@jcatlm.com.br>
2026-04-10 20:15:56 -0300

    Update 02_outlet_de_hoteis.cy.js

--- cypress/e2e/06_link_conexao/02_outlet_de_hoteis.cy.js
+++ cypress/e2e/06_link_conexao/02_outlet_de_hoteis.cy.js
@@ -1,12 +1,12 @@
-/// <reference types='cypress' />
+// /// <reference types='cypress' />
 
-describe('', () => {
-  beforeEach(() => {
-    cy.intercept({ resourceType: /xhr|fetch/ }, { log: false });
-    cy.visit('https://www.viacaocometa.com.br');
-  });
-  it('Deve validar o link do card de outlet de hotéis e redirecionar para a página do outlet de hotéis', () => {
-    cy.get('#slick-slide21 > .carousel-cards-content-text > .aem-Grid > .button > clientlib > .about-card-button > .focusable').invoke('removeAttr', 'target').click();
-    cy.url().should('include', 'www.outletdehoteis.com.br');
-  })
-});
+// describe('', () => {
+//   beforeEach(() => {
+//     cy.intercept({ resourceType: /xhr|fetch/ }, { log: false });
+//     cy.visit('https://www.viacaocometa.com.br');
+//   });
+//   it('Deve validar o link do card de outlet de hotéis e redirecionar para a página do outlet de hotéis', () => {
+//     cy.get('#slick-slide21 > .carousel-cards-content-text > .aem-Grid > .button > clientlib > .about-card-button > .focusable').invoke('removeAttr', 'target').click();
+//     cy.url().should('include', 'www.outletdehoteis.com.br');
+//   })
+// });
