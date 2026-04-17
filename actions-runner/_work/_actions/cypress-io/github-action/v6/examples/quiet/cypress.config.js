06242b95183d55d0cf091d520ddb07918e47f11d
Anderson Silva dos Santos <anderson.ssantos@jcatlm.com.br>
2026-04-17 11:09:56 -0300

    ajuestes

---
+++ actions-runner/_work/_actions/cypress-io/github-action/v6/examples/quiet/cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  fixturesFolder: false,
+  e2e: {
+    setupNodeEvents (on) {
+      on('task', {
+        log (message) {
+          console.log(message)
+          return null
+        },
+      })
+    },
+    supportFile: false,
+  },
+})
---
+++ actions-runner/_work/_actions/cypress-io/github-action/v6/examples/recording/cypress.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  fixturesFolder: false,
+  projectId: '3tb7jn',
+  e2e: {
+    supportFile: false,
+  },
+})
