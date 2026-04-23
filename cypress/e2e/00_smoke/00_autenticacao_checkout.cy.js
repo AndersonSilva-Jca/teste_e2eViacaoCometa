974961f26149da3a1dabbfdd7e89f1f068d05100
Anderson Silva dos Santos <anderson.ssantos@jcatlm.com.br>
2026-04-23 10:03:06 -0300

    Monitoramento Cypress - Viação Cometa

---
+++ cypress/e2e/00_smoke/00_autenticacao_checkout.cy.js
@@ -0,0 +1,29 @@
+/// <reference types='cypress' />
+
+import '../00_busca_passagens/busca_passagens.cy.js'
+
+import '../01_autenticacao/01_login.cy.js'
+
+import '../02_conta_giro/01_conta_giro.cy.js'
+
+// import '../03_links_header/01_Informacoes_viagens.cy.js'
+// // import './03_links_header/02_fale_conosco.cy.js'
+// import '../03_links_header/03_gratuidade.cy.js'
+// import '../03_links_header/04_nossos_destinos.cy.js'
+// import '../03_links_header/05_pontos_de_venda.cy.js'
+// import '../03_links_header/06_programa_fidelidade.cy.js'
+
+// // import './04_ofertas_promocoes/01_promo_cards.cy.js'
+
+// import '../05_link_conexao/01_clube_giro.cy.js'
+// import '../05_link_conexao/02_outlet_de_hoteis.cy.js'
+// import '../05_link_conexao/03_conexao_aeroportos.cy.js'
+// import '../05_link_conexao/04_conexao_portoSantos.cy.js'
+// import '../05_link_conexao/05_conheca_nossos_onibus.cy.js'
+
+// import '../06_cards_duvidas_frequentes/01_info_viagens.cy.js'
+// import '../06_cards_duvidas_frequentes/02_remarcacao.cy.js'
+// import '../06_cards_duvidas_frequentes/03_reembolso.cy.js'
+// import '../06_cards_duvidas_frequentes/04_bagagens.cy.js'
+
+// import '../07_links_footer/01_validar_links.cy.js'
---
+++ cypress/e2e/00_smoke/02_conexao_cards_footer.cy.js
@@ -0,0 +1,14 @@
+/// <reference types='cypress' />
+
+import '../05_link_conexao/01_clube_giro.cy.js'
+import '../05_link_conexao/02_outlet_de_hoteis.cy.js'
+import '../05_link_conexao/03_conexao_aeroportos.cy.js'
+import '../05_link_conexao/04_conexao_portoSantos.cy.js'
+import '../05_link_conexao/05_conheca_nossos_onibus.cy.js'
+
+import '../06_cards_duvidas_frequentes/01_info_viagens.cy.js'
+import '../06_cards_duvidas_frequentes/02_remarcacao.cy.js'
+import '../06_cards_duvidas_frequentes/03_reembolso.cy.js'
+import '../06_cards_duvidas_frequentes/04_bagagens.cy.js'
+
+import '../07_links_footer/01_validar_links.cy.js'
