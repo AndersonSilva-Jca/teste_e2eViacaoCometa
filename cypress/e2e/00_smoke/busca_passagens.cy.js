68a732ab28516c413738e0e8463e539b3a8317e5
Anderson Silva dos Santos <anderson.ssantos@jcatlm.com.br>
2026-04-27 14:33:55 -0300

    ajustes

--- cypress/e2e/00_smoke/busca_passagens.cy.js
+++ cypress/e2e/00_smoke/busca_passagens.cy.js
@@ -14,26 +14,26 @@ describe('Fazer busca de destinos, selecionar datas, compra de passagens, seleci
       cy.get(loc.USUARIO).type(env.login)
       cy.get(loc.SENHA).type(env.senha, { log: false })
       cy.get(loc.BOTAO_LOGIN).click()
-      cy.get(loc.MESSAGE_LOGGED).should('contain', 'Olá')
+      cy.get(loc.MENSAGEM_LOGADO).should('contain', 'Olá')
     })
   });
-  it('Fazer busca de destinos IDA com 1 passageiro', () => {
-    cy.get(loc.DESTINO_IDA).click().type('São Paulo (Todos) (SP)', {delay: 100}).should('exist').invoke('show')
+  it.only('Fazer busca de destinos IDA com 1 passageiro', () => {
+    cy.get(loc.BUSCAS.DESTINO_IDA).click().type('São Paulo (Todos) (SP)', {delay: 100}).should('exist').invoke('show')
     cy.contains(' São Paulo (Todos) (SP) ').click({ force: true })
-    cy.get(loc.DESTINO_VOLTA).click().type('Rio de Janeiro (Todos) (RJ)', {delay: 100}).should('exist').invoke('show')
+    cy.get(loc.BUSCAS.DESTINO_VOLTA).click().type('Rio de Janeiro (Todos) (RJ)', {delay: 100}).should('exist').invoke('show')
     cy.contains(' Rio de Janeiro (Todos) (RJ) ').click({ force: true })
-    cy.get(loc.DATA_IDA).click()
+    cy.get(loc.BUSCAS.DATA_IDA).click()
     cy.selecionarDataIda(2)
-    cy.get(loc.BOTAO_BUSCAR, { timeout: 90000 }).should('be.visible').click()
+    cy.get(loc.BUSCAS.BOTAO_BUSCAR, { timeout: 90000 }).should('be.visible').click()
     cy.wait(5000)
     cy.selecionarPassagemAleatoria1({ timeout: 90000 })
     cy.wait(2000)
-    cy.get('#buyer-check-1', { timeout: 90000 }).click({ force: true })
-    cy.get('#btn-proceed').should('be.visible').and('not.be.disabled').click();
+    cy.get(loc.CHECK_PASSAGEIRO, { timeout: 90000 }).click({ force: true })
+    cy.get(loc.BOTAO_AVANCAR).should('be.visible').and('not.be.disabled').click();
     cy.contains('Escolha o seu assento', { timeout: 90000 }).should('be.visible');
     cy.selecionarAssentoAleatorio({ timeout: 90000 });
-    cy.get('#btn-proceed').should('be.visible').click()
-    cy.url({ timeout: 90000 }).should('include', '/pagamento')
+    cy.get(loc.BOTAO_AVANCAR).should('be.visible').click()
+    // cy.url({ timeout: 90000 }).should('include', '/pagamento')
     // Não finalizar a compra para evitar transações reais
     // cy.get('#tab-pix').click()
     // cy.get('.conditions-check', { timeout: 20000 }).click({ force: true })
--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -1,14 +1,15 @@
-const locators = {
-  LOGIN: {
+const loc = {
+
     HEADER_BOTAO_LOGIN: '#header-login-button',
     USUARIO: '#input-login',
     SENHA: '#input-password',
     BOTAO_LOGIN: '#button-login',
-  },
-  MENSAGEM: {
+ 
     MENSAGEM_LOGADO: '.logged-message',
+    CHECK_PASSAGEIRO: '#buyer-check-1',
+    BOTAO_AVANCAR: '#btn-proceed',
 
-  },
+ 
   BUSCAS: {
     DESTINO_IDA: '#input-departure',
     DESTINO_VOLTA: '#input-destination',
@@ -20,4 +21,4 @@ const locators = {
 
 }
 
-export default locators;
+export default loc;
