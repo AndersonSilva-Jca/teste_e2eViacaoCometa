87172599eaa3fcc77d4ca82bb808e4bc9d7fb654
Anderson Silva dos Santos <anderson.ssantos@jcatlm.com.br>
2026-05-07 13:39:27 -0300

    Monitoramento

--- cypress/e2e/00_smoke/02_busca_passagens.cy.js
+++ cypress/e2e/00_smoke/02_busca_passagens.cy.js
@@ -130,94 +130,94 @@ describe('Viação Cometa - Fazer busca de destinos, selecionar datas, compra de
     // cy.get('#payment-submit').should('be.visible').and('not.be.disabled').click();
   })
 
-  it('Fazer busca de destinos IDA e Volta com 1 passageiro', () => {
-       cy.env(['login', 'senha']).then((env) => {
-      cy.get(loc.HEADER_BOTAO_LOGIN).click()
-      cy.get(loc.USUARIO).type(env.login)
-      cy.get(loc.SENHA).type(env.senha, { log: false })
-      cy.get(loc.BOTAO_LOGIN).click()
-      cy.get(loc.MENSAGEM_LOGADO).if('not.be.visible').get('.normal').should('contain', 'O email ou senha inseridos não constam em nosso cadastro').else().log('Login realizado com sucesso').should('contain', 'Olá')
-    })
-    cy.get(loc.BUSCAS.DESTINO_IDA).click().type('São Paulo (Todos) (SP)', {delay: 100}).should('exist').invoke('show')
-    cy.contains(' São Paulo (Todos) (SP) ').click().should('exist').invoke('show')
-    cy.get(loc.BUSCAS.DESTINO_VOLTA).click().type('Rio de Janeiro (Todos) (RJ)', {delay: 100}).should('exist').invoke('show')
-    cy.contains(' Rio de Janeiro (Todos) (RJ) ').click().should('exist').invoke('show')
-    cy.get(loc.BUSCAS.DATA_IDA).click()
-    cy.selecionarDataIda(1)
-    cy.get(loc.BUSCAS.DATA_VOLTA).click()
-    cy.selecionarDataVolta(6)
-    cy.get(loc.BUSCAS.BOTAO_BUSCAR).click()
-    cy.wait(5000)
-    cy.selecionarPassagemAleatoria1({ timeout: 90000 })
-    cy.contains('ESCOLHER PASSAGENS', { timeout: 90000 }).should('be.visible');
-    cy.wait(25000)
-    cy.selecionarPassagemAleatoria1({ timeout: 90000 })
-    // cy.wait(15000)
-    cy.get(loc.CHECK_PASSAGEIRO, { timeout: 60000 }).click({ force: true })
-    cy.get(loc.BOTAO_AVANCAR).should('be.visible').and('not.be.disabled').click({ force: true });
-    cy.url({ timeout: 60000 }).should('include', '/poltrona')
-    cy.wait(8000)
-    cy.contains('Escolha o seu assento', { timeout: 20000 }).should('be.visible')
-    cy.selecionarAssentoAleatorio1({ timeout: 15000 });
-    cy.wait(8000)
-    cy.get(loc.BOTAO_AVANCAR).should('be.visible').and('not.be.disabled').click({ force: true });
-    cy.url({ timeout: 60000 }).should('include', '/poltrona')
-    cy.wait(8000)
-    cy.selecionarAssentoAleatorio1({ timeout: 15000 });
-    cy.get(loc.BOTAO_AVANCAR).should('be.visible').and('not.be.disabled').click({ force: true });
-    cy.url({ timeout: 60000 }).should('include', '/pagamento')
-    // cy.get('[alt="loader"]').should('not.exist')
-    //   cy.get('#tab-pix').click() 
-    //   cy.get('.conditions-check', { timeout: 20000 }).click({ force: true })
-    // Não finalizar a compra para evitar transações reais
-    // cy.get('#payment-submit').should('be.visible').and('not.be.disabled').click();
-  })
+  // it('Fazer busca de destinos IDA e Volta com 1 passageiro', () => {
+  //      cy.env(['login', 'senha']).then((env) => {
+  //     cy.get(loc.HEADER_BOTAO_LOGIN).click()
+  //     cy.get(loc.USUARIO).type(env.login)
+  //     cy.get(loc.SENHA).type(env.senha, { log: false })
+  //     cy.get(loc.BOTAO_LOGIN).click()
+  //     cy.get(loc.MENSAGEM_LOGADO).if('not.be.visible').get('.normal').should('contain', 'O email ou senha inseridos não constam em nosso cadastro').else().log('Login realizado com sucesso').should('contain', 'Olá')
+  //   })
+  //   cy.get(loc.BUSCAS.DESTINO_IDA).click().type('São Paulo (Todos) (SP)', {delay: 100}).should('exist').invoke('show')
+  //   cy.contains(' São Paulo (Todos) (SP) ').click().should('exist').invoke('show')
+  //   cy.get(loc.BUSCAS.DESTINO_VOLTA).click().type('Rio de Janeiro (Todos) (RJ)', {delay: 100}).should('exist').invoke('show')
+  //   cy.contains(' Rio de Janeiro (Todos) (RJ) ').click().should('exist').invoke('show')
+  //   cy.get(loc.BUSCAS.DATA_IDA).click()
+  //   cy.selecionarDataIda(1)
+  //   cy.get(loc.BUSCAS.DATA_VOLTA).click()
+  //   cy.selecionarDataVolta(6)
+  //   cy.get(loc.BUSCAS.BOTAO_BUSCAR).click()
+  //   cy.wait(5000)
+  //   cy.selecionarPassagemAleatoria1({ timeout: 90000 })
+  //   cy.contains('ESCOLHER PASSAGENS', { timeout: 90000 }).should('be.visible');
+  //   cy.wait(25000)
+  //   cy.selecionarPassagemAleatoria1({ timeout: 90000 })
+  //   // cy.wait(15000)
+  //   cy.get(loc.CHECK_PASSAGEIRO, { timeout: 60000 }).click({ force: true })
+  //   cy.get(loc.BOTAO_AVANCAR).should('be.visible').and('not.be.disabled').click({ force: true });
+  //   cy.url({ timeout: 60000 }).should('include', '/poltrona')
+  //   cy.wait(8000)
+  //   cy.contains('Escolha o seu assento', { timeout: 20000 }).should('be.visible')
+  //   cy.selecionarAssentoAleatorio1({ timeout: 15000 });
+  //   cy.wait(8000)
+  //   cy.get(loc.BOTAO_AVANCAR).should('be.visible').and('not.be.disabled').click({ force: true });
+  //   cy.url({ timeout: 60000 }).should('include', '/poltrona')
+  //   cy.wait(8000)
+  //   cy.selecionarAssentoAleatorio1({ timeout: 15000 });
+  //   cy.get(loc.BOTAO_AVANCAR).should('be.visible').and('not.be.disabled').click({ force: true });
+  //   cy.url({ timeout: 60000 }).should('include', '/pagamento')
+  //   // cy.get('[alt="loader"]').should('not.exist')
+  //   //   cy.get('#tab-pix').click() 
+  //   //   cy.get('.conditions-check', { timeout: 20000 }).click({ force: true })
+  //   // Não finalizar a compra para evitar transações reais
+  //   // cy.get('#payment-submit').should('be.visible').and('not.be.disabled').click();
+  // })
 
-  it('Fazer busca de destinos IDA e Volta com 2 passageiros', () => {
-      cy.env(['login1', 'senha1']).then((env) => {
-      cy.get(loc.HEADER_BOTAO_LOGIN).click()
-      cy.get(loc.USUARIO).type(env.login1)
-      cy.get(loc.SENHA).type(env.senha1, { log: false })
-      cy.get(loc.BOTAO_LOGIN).click()
-      cy.get(loc.MENSAGEM_LOGADO).if('not.be.visible').get('.normal').should('contain', 'O email ou senha inseridos não constam em nosso cadastro').else().log('Login realizado com sucesso').should('contain', 'Olá')
-    })
-    cy.get(loc.BUSCAS.DESTINO_IDA).click().type('São Paulo (Todos) (SP)', {delay: 100}).should('exist').invoke('show')
-    cy.contains(' São Paulo (Todos) (SP) ').click()
-    cy.get(loc.BUSCAS.DESTINO_VOLTA).click().type('Rio de Janeiro (Todos) (RJ)', {delay: 100}).should('exist').invoke('show')
-    cy.contains(' Rio de Janeiro (Todos) (RJ) ').click()
-    cy.get(loc.BUSCAS.DATA_IDA).click()
-    cy.selecionarDataIda(1)
-    cy.get(loc.BUSCAS.DATA_VOLTA).click()
-    cy.selecionarDataVolta(6)
-    cy.get(loc.BUSCAS.BOTAO_PASSAGEIROS).click()
-    cy.get(loc.BUSCAS.BOTAO_ADICIONAR_PASSAGEIRO).click()
-    cy.get(loc.BUSCAS.BOTAO_FECHAR_QUANTIDADE_PASSAGEIROS).click()
-    cy.get(loc.BUSCAS.BOTAO_BUSCAR).click()
-    // cy.wait(5000)
-    cy.selecionarPassagemAleatoria1({ timeout: 90000 })
-    cy.wait(30000)
-    cy.selecionarPassagemAleatoria1({ timeout: 90000 })
-    // cy.wait(5000)
-    cy.get(loc.CHECK_PASSAGEIRO, { timeout: 20000 }).click({ force: true })
-    cy.get(loc.NOME_PASSAGEIRO_2).click()
-    cy.contains('Teste Robo ODP').should('be.visible').click()
-    cy.get(loc.DATA_ANIVERSARIO_PASSAGEIRO_2).type('01011990')
-    cy.get(loc.CLICK_PASSAGEIROS).click()
-    cy.get(loc.BOTAO_AVANCAR).should('be.visible').and('not.be.disabled').click();
-    cy.contains('Escolha o seu assento', { timeout: 20000 }).should('be.visible')
-    cy.wait(2000)
-    cy.selecionarDoisAssentosAleatorios({ timeout: 90000 });
-    cy.get(loc.BOTAO_AVANCAR, { timeout: 90000 }).should('be.visible').click()
-    cy.contains('Escolha o seu assento', { timeout: 90000 }).should('be.visible')
-    cy.wait(2000)
-    cy.selecionarDoisAssentosAleatorios({ timeout: 90000 });
-    cy.get(loc.BOTAO_AVANCAR, { timeout: 90000 }).should('be.visible').click();
-    // cy.get('[alt="loader"]').should('not.exist')
-    // cy.wait(5000)
-    // cy.get('#tab-pix').click()
-    // cy.get('.conditions-check', { timeout: 20000 }).click({ force: true })
-    //  Não finalizar a compra para evitar transações reais
-    // cy.get('#payment-submit').should('be.visible').and('not.be.disabled').click();
-  })
+  // it('Fazer busca de destinos IDA e Volta com 2 passageiros', () => {
+  //     cy.env(['login1', 'senha1']).then((env) => {
+  //     cy.get(loc.HEADER_BOTAO_LOGIN).click()
+  //     cy.get(loc.USUARIO).type(env.login1)
+  //     cy.get(loc.SENHA).type(env.senha1, { log: false })
+  //     cy.get(loc.BOTAO_LOGIN).click()
+  //     cy.get(loc.MENSAGEM_LOGADO).if('not.be.visible').get('.normal').should('contain', 'O email ou senha inseridos não constam em nosso cadastro').else().log('Login realizado com sucesso').should('contain', 'Olá')
+  //   })
+  //   cy.get(loc.BUSCAS.DESTINO_IDA).click().type('São Paulo (Todos) (SP)', {delay: 100}).should('exist').invoke('show')
+  //   cy.contains(' São Paulo (Todos) (SP) ').click()
+  //   cy.get(loc.BUSCAS.DESTINO_VOLTA).click().type('Rio de Janeiro (Todos) (RJ)', {delay: 100}).should('exist').invoke('show')
+  //   cy.contains(' Rio de Janeiro (Todos) (RJ) ').click()
+  //   cy.get(loc.BUSCAS.DATA_IDA).click()
+  //   cy.selecionarDataIda(1)
+  //   cy.get(loc.BUSCAS.DATA_VOLTA).click()
+  //   cy.selecionarDataVolta(6)
+  //   cy.get(loc.BUSCAS.BOTAO_PASSAGEIROS).click()
+  //   cy.get(loc.BUSCAS.BOTAO_ADICIONAR_PASSAGEIRO).click()
+  //   cy.get(loc.BUSCAS.BOTAO_FECHAR_QUANTIDADE_PASSAGEIROS).click()
+  //   cy.get(loc.BUSCAS.BOTAO_BUSCAR).click()
+  //   // cy.wait(5000)
+  //   cy.selecionarPassagemAleatoria1({ timeout: 90000 })
+  //   cy.wait(30000)
+  //   cy.selecionarPassagemAleatoria1({ timeout: 90000 })
+  //   // cy.wait(5000)
+  //   cy.get(loc.CHECK_PASSAGEIRO, { timeout: 20000 }).click({ force: true })
+  //   cy.get(loc.NOME_PASSAGEIRO_2).click()
+  //   cy.contains('Teste Robo ODP').should('be.visible').click()
+  //   cy.get(loc.DATA_ANIVERSARIO_PASSAGEIRO_2).type('01011990')
+  //   cy.get(loc.CLICK_PASSAGEIROS).click()
+  //   cy.get(loc.BOTAO_AVANCAR).should('be.visible').and('not.be.disabled').click();
+  //   cy.contains('Escolha o seu assento', { timeout: 20000 }).should('be.visible')
+  //   cy.wait(2000)
+  //   cy.selecionarDoisAssentosAleatorios({ timeout: 90000 });
+  //   cy.get(loc.BOTAO_AVANCAR, { timeout: 90000 }).should('be.visible').click()
+  //   cy.contains('Escolha o seu assento', { timeout: 90000 }).should('be.visible')
+  //   cy.wait(2000)
+  //   cy.selecionarDoisAssentosAleatorios({ timeout: 90000 });
+  //   cy.get(loc.BOTAO_AVANCAR, { timeout: 90000 }).should('be.visible').click();
+  //   // cy.get('[alt="loader"]').should('not.exist')
+  //   // cy.wait(5000)
+  //   // cy.get('#tab-pix').click()
+  //   // cy.get('.conditions-check', { timeout: 20000 }).click({ force: true })
+  //   //  Não finalizar a compra para evitar transações reais
+  //   // cy.get('#payment-submit').should('be.visible').and('not.be.disabled').click();
+  // })
 
 })
