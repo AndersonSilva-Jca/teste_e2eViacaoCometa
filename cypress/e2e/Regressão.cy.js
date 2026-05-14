d87030456e0f4d3317d2143f2be7394f1ce7b117
Anderson Silva dos Santos <anderson.ssantos@jcatlm.com.br>
2026-05-14 09:43:57 -0300

    Refatorando

--- cypress/e2e/Regressão.cy.js
+++ cypress/e2e/Regressão.cy.js
@@ -2,8 +2,8 @@
 
 import "./00_smoke/01_utp_busca_passagens.cy.js";
 import "./00_smoke/02_busca_passagens.cy.js";
-import "./01_autenticacao/login.cy.js";
-import "./02_conta_giro/conta_giro.cy.js";
+// import "./01_autenticacao/login.cy.js";
+// import "./02_conta_giro/conta_giro.cy.js";
 import "./03_links_header/links_header.cy.js";
 import "./04_ofertas_promocoes/promo_cards.cy.js";
 import "./05_link_conexao/link_conexao.cy.js";
